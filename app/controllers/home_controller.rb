9fa6392764a2daa7dccdc4717b0dffeb8bd9d205
Theodor Vararu <theo@vararu.org>
2017-07-19 12:39:49 +0100

    Add show picker page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -12,6 +12,14 @@ class HomeController < ApplicationController
     @register = OpenRegister.register(@register_name, @register_phase.to_sym)
   end
 
+  def show_picker
+    @register_name = params[:selected_register]
+    @register_phase = params[:selected_phase]
+    @register_field = params[:selected_field]
+
+    @register_records = OpenRegister.register(@register_name, @register_phase.to_sym)._all_records
+  end
+
   def initializeRegisters()
     list_of_registers = OpenRegister.registers :beta
     list_of_registers.concat(OpenRegister.registers :alpha)
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root 'home#index'
   post 'choose-field', to: 'home#choose_field'
+  post 'show-picker', to: 'home#show_picker'
 end
