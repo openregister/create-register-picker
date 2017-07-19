780ed577085402e02980f1096ae98192923289b0
Theodor Vararu <theo@vararu.org>
2017-07-19 11:00:38 +0100

    Add autocomplete and form submit to choose a register page

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,4 +7,15 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
-console.log('Hello World from Webpacker')
+import accessibleAutocomplete from 'accessible-autocomplete'
+import 'accessible-autocomplete/dist/accessible-autocomplete.min.css'
+
+const $listOfRegistersEl = document.querySelector('#list-of-registers')
+
+if ($listOfRegistersEl) {
+  accessibleAutocomplete.enhanceSelectElement({
+    autoselect: false,
+    selectElement: $listOfRegistersEl,
+    showAllValues: true
+  })
+}
