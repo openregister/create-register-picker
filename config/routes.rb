6d1a9d1fea9e869ffdabfdad7c09f5b4be4d97a4
Dan Mitchell <dan@digitalreflow.co.uk>
2017-08-01 13:22:56 +0100

    Add static download page with code syntax highlighting

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root 'home#index'
   post 'choose-field', to: 'home#choose_field'
   post 'show-picker', to: 'home#show_picker'
+  get 'download', to: 'home#download'
 end
